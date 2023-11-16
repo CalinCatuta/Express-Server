cf702c13c604b2a475b51785a629183d4115410f
CalinCatuta <calinut123@yahoo.co.uk>
2023-11-16 18:41:49 +0200

    put and delete

--- routes/ideas.js
+++ routes/ideas.js
@@ -45,5 +45,38 @@ router.get("/:id", (req, res) => {
   }
   res.json({ succes: true, data: idea });
 });
+// Add an idea
+router.post("/", (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    username: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  };
+  ideas.push(idea);
+  res.json({ succes: true, data: idea });
+});
+
+//update idea
+router.put("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+  if (!idea) {
+    return res.status(404).json({ succes: false, error: "Not found" });
+  }
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+  res.json({ succes: true, data: idea });
+});
+// delet idea
+router.delete("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+  if (!idea) {
+    return res.status(404).json({ succes: false, error: "Not found" });
+  }
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+  res.json({ succes: true, data: {} });
+});
 
 module.exports = router;
